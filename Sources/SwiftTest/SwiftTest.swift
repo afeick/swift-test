c21d95fac663f7e6e57257b57f960185f1a648bf
Tony Feick <tony.feick@jumpcloud.com>
2023-04-17 17:03:20 -0600

    add some lint issues

--- Sources/SwiftTest/SwiftTest.swift
+++ Sources/SwiftTest/SwiftTest.swift
@@ -1,6 +1,8 @@
+import Foundation
+
 /// Boilerplate generated library for testing github actions
 public struct SwiftTest {
-    public private(set) var text: String
+    public private(set) var text : String
 
     public init(_ message: String = "Hello, World!") {
         text = message
