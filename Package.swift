73c0e2af8a7c5f4009c6a3c3987bed537bfce1d0
Tony Feick <tony.feick@jumpcloud.com>
2023-04-18 17:17:38 -0600

    add a dependency to validate the action

--- Package.swift
+++ Package.swift
@@ -12,15 +12,14 @@ let package = Package(
             targets: ["SwiftTest"])
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "git@github.com:/apple/swift-log.git", exact: "1.4.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwiftTest",
-            dependencies: []),
+            dependencies: [.product(name: "Logging", package: "swift-log")]),
         .testTarget(
             name: "SwiftTestTests",
             dependencies: ["SwiftTest"])
--- Sources/SwiftTest/SwiftTest.swift
+++ Sources/SwiftTest/SwiftTest.swift
@@ -1,8 +1,12 @@
+import Logging
+
 /// Boilerplate generated library for testing github actions
 public struct SwiftTest {
     public private(set) var text: String
 
     public init(_ message: String = "Hello, World!") {
+        let logger = Logger(label: "SwiftTest")
+        logger.info("message: \(message)")
         text = message
     }
 }
