6bf5be5344214e953457d031bb51455fc30afa02
Tony Feick <tony.feick@jumpcloud.com>
2023-04-25 22:51:34 -0600

    whatever

--- Tests/SwiftTestTests/SwiftTestTests.swift
+++ Tests/SwiftTestTests/SwiftTestTests.swift
@@ -6,8 +6,8 @@ final class SwiftTestTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(SwiftTest().text, "Hello, World!!!")
-        XCTAssertEqual(SwiftTest("Hello, Bitches!!!").text, "Hello, Bitches!!!")
+        XCTAssertEqual(SwiftTest().text, "Hello, World!")
+        XCTAssertEqual(SwiftTest("Hello, Bitches!").text, "Hello, Bitches!")
         // adding a comment
     }
 }
